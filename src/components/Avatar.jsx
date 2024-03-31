c23cd25f684548a2cc2df2c303bd246f8a386691
alpaca <alpteo13@gmail.com>
2024-04-01 01:56:28 +0800

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -24,7 +24,7 @@ function HorizontalOrbitControls(props) {
 
 
 export function Avatar(props) {
-  const { nodes, materials } = useGLTF('../public/models/Waving.glb')
+  const { nodes, materials } = useGLTF('/public/models/Waving.glb')
   return (
     <Canvas shadows camera={{ position: [0, 400, 900], fov: 45 }} style={{ width: '100%', height: '60vh' }}>
     <Environment preset ="sunset"/>
