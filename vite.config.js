6f8402e1aea91747bce2498f20853865601f4135
alpaca <alpteo13@gmail.com>
2024-03-31 18:22:55 +0800

    Deploy Site

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Adjust this value as needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example of manual code splitting based on vendor modules
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  server: {
+    compress: true, // Enable gzip compression
+  },
 })
