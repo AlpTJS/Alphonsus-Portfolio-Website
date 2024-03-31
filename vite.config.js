495e9634edc69f1f26a16f7ba8961d7473825ee3
alpaca <alpteo13@gmail.com>
2024-04-01 01:04:31 +0800

    Editing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Alphonsus-Portfolio-Website/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1500, // Adjust this value as needed
